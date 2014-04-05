9726e7074553b7639af42c268c068441218bfa4a
gyfnice <751143842@qq.com>
2014-04-06 02:31:32 +0800

    Update testType.js

--- test/testType.js
+++ test/testType.js
@@ -6,7 +6,7 @@ var ClassId = [];
 describe('test Type', function() {
 	var typeArray = ["运动", "聚会", "娱乐", "文艺", "其它"];
 	describe('#insert', function() {
-		it('should return null when the value is inserted', function() {
+		it('should return null when the value is inserted', function(done) {
 			typeArray.forEach(function(elem) {
 				new Classify(elem).save(function(err,result) {
 					 ClassId.push(result._id.valueOf());
@@ -16,13 +16,15 @@ describe('test Type', function() {
                                    assert.equal(ClassId[0],undefined);
 				});
 			})
+                     done();
 		})
 	});
-	/*describe('#find',function () {
-	    it('should return one result by pass id',function(){
+	describe('#find',function () {
+	    it('should return one result by pass id',function(done){
 	    	Classify.get(ClassId[0],function(err,res){
 	    		assert.equal(res.name,"运动");
 	    	});
+              done();
 	    })
-	})*/
+	})
 })
